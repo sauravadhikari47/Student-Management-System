29996aa182c2d8b85a3ce68d5e4c28c959c6266e
Saurav Adhikari <sauravadhikari47@gmail.com>
2017-06-22 21:05:51 +0545

    Add files via upload

---
+++ Settings.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.sql.*;
+public class Settings {
+		public static Connection getDBCOnnection() {
+		Connection con;
+		try{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost/studentd","root","");
+			return con;
+			
+		}catch(Exception e){
+			return null;
+		}
+		
+	}
+
+}
